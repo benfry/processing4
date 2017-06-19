benfry/processing4

commit d960fd1cba7f8ef00d6e959477ca98c150b31a7a

prevent "run sketches on display" message from showing up on startup

java/src/processing/mode/java/runner/Runner.java
@@ -376,7 +376,9 @@ public class Runner implements MessageConsumer {
     } else {
       params.append("processing.core.PApplet");
 
-      // get the stored device index (starts at 1)
+      // Get the stored device index (starts at 1)
+      // By default, set to -1, meaning 'the default display',
+      // which is the same display as the one being used by the Editor.
       int runDisplay = Preferences.getInteger("run.display");
 
       // If there was a saved location (this guy has been run more than once)
@@ -395,23 +397,26 @@ public class Runner implements MessageConsumer {
 
         // Make sure the display set in Preferences actually exists
         GraphicsDevice runDevice = editorDevice;
-        if (runDisplay > 0 && runDisplay <= devices.length) {
-          runDevice = devices[runDisplay-1];
-        } else {
-          // If a bad display is selected, use the same display as the editor
-          if (runDisplay > 0) {  // don't complain about -1 or 0
-            System.err.println("Display " + runDisplay + " not available.");
-          }
-          runDevice = editorDevice;
-          for (int i = 0; i < devices.length; i++) {
-            if (devices[i] == runDevice) {
-              // Wasn't setting the pref to avoid screwing things up with
-              // something temporary. But not setting it makes debugging one's
-              // setup just too damn weird, so changing that behavior.
-              runDisplay = i + 1;
-              System.err.println("Setting 'Run Sketches on Display' preference to display " + runDisplay);
-              Preferences.setInteger("run.display", runDisplay);
-              break;
+        if (runDisplay > 0) {
+          if (runDisplay <= devices.length) {
+            runDevice = devices[runDisplay-1];
+
+          } else {
+            // If a bad display is selected, use the same display as the editor
+            if (runDisplay > 0) {  // don't complain about -1 or 0
+              System.err.println("Display " + runDisplay + " not available.");
+            }
+            runDevice = editorDevice;
+            for (int i = 0; i < devices.length; i++) {
+              if (devices[i] == runDevice) {
+                // Wasn't setting the pref to avoid screwing things up with
+                // something temporary. But not setting it makes debugging one's
+                // setup just too damn weird, so changing that behavior.
+                runDisplay = i + 1;
+                System.err.println("Setting 'Run Sketches on Display' preference to display " + runDisplay);
+                Preferences.setInteger("run.display", runDisplay);
+                break;
+              }
             }
           }
         }

commit 5e6a147896e8523f2564eea9e80d1723a8be48dd

Fix "Could not parse -1 for --dsiplay" error
When runDisplay was set to -1 no display detection happened. Fixed it so
that editor display is used.

Regression introduced in d960fd1cba7f8ef00d6e959477ca98c150b31a7a

Fixes #5118

java/src/processing/mode/java/runner/Runner.java
@@ -400,26 +400,26 @@ public class Runner implements MessageConsumer {
 
         // Make sure the display set in Preferences actually exists
         GraphicsDevice runDevice = editorDevice;
-        if (runDisplay > 0) {
-          if (runDisplay <= devices.length) {
+        if (runDisplay > 0 && runDisplay <= devices.length) {
             runDevice = devices[runDisplay-1];
-
-          } else {
-            // If a bad display is selected, use the same display as the editor
-            if (runDisplay > 0) {  // don't complain about -1 or 0
-              System.err.println("Display " + runDisplay + " not available.");
-            }
-            runDevice = editorDevice;
-            for (int i = 0; i < devices.length; i++) {
-              if (devices[i] == runDevice) {
-                // Wasn't setting the pref to avoid screwing things up with
-                // something temporary. But not setting it makes debugging one's
-                // setup just too damn weird, so changing that behavior.
-                runDisplay = i + 1;
-                System.err.println("Setting 'Run Sketches on Display' preference to display " + runDisplay);
-                Preferences.setInteger("run.display", runDisplay);
-                break;
+        } else {
+          // If a bad display (or -1 display) is selected, use the same display as the editor
+          if (runDisplay > 0) {  // don't complain about -1 or 0
+            System.err.println("Display " + runDisplay + " not available.");
+          }
+          runDevice = editorDevice;
+          for (int i = 0; i < devices.length; i++) {
+            if (devices[i] == runDevice) {
+              // Prevent message on the first run
+              if (runDisplay != -1) {
+                System.err.println("Setting 'Run Sketches on Display' preference to display " + (i+1));
               }
+              runDisplay = i + 1;
+              // Wasn't setting the pref to avoid screwing things up with
+              // something temporary. But not setting it makes debugging one's
+              // setup just too damn weird, so changing that behavior.
+              Preferences.setInteger("run.display", runDisplay);
+              break;
             }
           }
         }