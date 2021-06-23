26f01a539fa5d9e2059e156a12c4e58f436af114
Ben Fry <ben@fathom.info>
2021-06-23 08:29:55 -0400

    a note about license, and testing "extends PSketch"

--- core/src/processing/core/PApplet.java
+++ core/src/processing/core/PApplet.java
@@ -82,6 +82,7 @@ import processing.opengl.*;
  * than working around legacy Java code.
  */
 public class PApplet implements PConstants {
+//public class PApplet extends PSketch {  // possible in the next alpha
   /** Full name of the Java version (i.e. 1.5.0_11). */
   static public final String javaVersionName =
     System.getProperty("java.version");
---
+++ core/src/processing/core/PSketch.java
@@ -0,0 +1,6 @@
+package processing.core;
+
+
+public class PSketch implements PConstants {
+    // this is a dummy class
+}
