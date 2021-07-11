a1122aa674fb41c91ebea7e38eb7a7cae197a0e5
Ben Fry <ben@fathom.info>
2021-07-11 07:23:46 -0400

    rolling over for the next release

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -51,9 +51,9 @@ import processing.data.StringList;
 public class Base {
   // Added accessors for 0218 because the UpdateCheck class was not properly
   // updating the values, due to javac inlining the static final values.
-  static private final int REVISION = 1275;
+  static private final int REVISION = 1276;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static private String VERSION_NAME = "1275"; //$NON-NLS-1$
+  static private String VERSION_NAME = "1276"; //$NON-NLS-1$
 
   /**
    * True if heavy debugging error/log messages are enabled. Set to true
