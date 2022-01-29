6ec1887eba274ae279fcf743a58c354a832377b0
Ben Fry <ben@fathom.info>
2022-01-29 08:16:31 -0500

    working a bit on debug

--- app/src/processing/app/syntax/SyntaxDebug.java
+++ app/src/processing/app/syntax/SyntaxDebug.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 
 import processing.app.Platform;
 import processing.app.Preferences;
+import processing.app.ui.Editor;
 import processing.app.ui.Theme;
 
 
@@ -25,10 +26,26 @@ public class SyntaxDebug extends JFrame {
     add(area);
     //setMinimumSize(new Dimension(500, 400));
     pack();
+    setLocationRelativeTo(null);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     setMinimumSize(new Dimension(500, 400));
     setVisible(true);
   }
+
+
+  /*
+  void drawError() {
+    PdeTextAreaPainter painter = (PdeTextAreaPainter) area.getPainter();
+
+    int x1 = area.offsetToX(line, leftCol);
+    int x2 = area.offsetToX(line, rightCol);
+    //if (x1 == x2) x2 += fontMetrics.stringWidth(" ");
+
+    gfx.setColor(painter.errorUnderlineColor);
+    //gfx.setColor(painter.warningUnderlineColor);
+    painter.paintSquiggle(gfx, y1, x1, x2);
+  }
+  */
 
 
   static public void main(String[] args) {
