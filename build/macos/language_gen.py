84aa7f920197f246d2511ba90a01c178782678f9
Ben Fry <ben@fathom.info>
2022-11-25 08:45:13 -0500

    rebuild script to better support multiple options and simplify

--- build/macos/language_gen.py
+++ build/macos/language_gen.py
@@ -2,25 +2,26 @@
 
 import os, re
 
-BASEDIR = os.path.dirname(os.path.realpath(__file__))
+HERE = os.path.dirname(os.path.realpath(__file__))
+
 
 def supported_languages():
-    path = "../../build/shared/lib/languages/languages.txt"
-    with open(os.path.join(BASEDIR, path)) as f:
-        lines = f.read().splitlines()
+    outgoing = set()
+    translations_path = os.path.join(HERE, '../shared/lib/languages')
+    with os.scandir(translations_path) as it:
+        for entry in it:
+            if entry.name.startswith('PDE_') and entry.name.endswith('.properties'):
+                outgoing.add(entry.name[4:-11])
+    return outgoing
 
-    lines = filter(lambda l: re.match(r'^[a-z]{2}', l), lines)
-    lines = map(lambda l: re.sub(r'#.*', '', l).strip(), lines)
-    return lines
 
 def lproj_directory(lang):
     path = "work/Processing.app/Contents/Resources/{}.lproj".format(lang)
-    return os.path.join(BASEDIR, path)
+    return os.path.join(HERE, path)
 
 
 if __name__ == "__main__":
     for lang in supported_languages():
-        try:
-            os.mkdir(lproj_directory(lang))
-        except OSError:
-            pass
+        path = lproj_directory(lang)
+        if not os.path.exists(path):
+            os.makedirs(path)
