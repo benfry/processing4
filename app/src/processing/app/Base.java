f71ecce2238d5f0cb8787a002d994ec7f586b19f
Ben Fry <ben@fathom.info>
2022-11-28 10:42:24 -0500

    rolling over for next release

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -54,9 +54,9 @@ import processing.data.StringList;
 public class Base {
   // Added accessors for 0218 because the UpdateCheck class was not properly
   // updating the values, due to javac inlining the static final values.
-  static private final int REVISION = 1289;
+  static private final int REVISION = 1290;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static private String VERSION_NAME = "1289"; //$NON-NLS-1$
+  static private String VERSION_NAME = "1290"; //$NON-NLS-1$
 
   static final public String SKETCH_BUNDLE_EXT = ".pdez";
   static final public String CONTRIB_BUNDLE_EXT = ".pdex";
