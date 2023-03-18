4d75b859bace5a1b8d950cd63865268cdf95c4e4
lala-lala-lori <ajit99@jnu.ac.in>
2023-03-18 10:40:42 +0530

    updated pde-handler.sh

--- build/linux/pde-handler.sh
+++ build/linux/pde-handler.sh
@@ -2,16 +2,17 @@
 
 # Support for pde browser protocol
 # Processing executable path
-EXEC=<BINARY_LOCATION>
+EXEC=/home/deidara/dev/processing4/build/linux/work/processing
 
-if [ -z $1 ]; then
+if [ -z "${1}" ]; then
     "$EXEC"
     exit 0
 fi
 
 if [[ "$1" == "pde:"* ]]; then
     # Extracting *.pde file path from url
-    file_path=$(echo "$1" | sed 's|pde://||')
+    url=$(echo "$1" | sed 's|pde://||')
+    file_path=$(python3 -c "import urllib.parse; print(urllib.parse.unquote('$url'))")
 else
     file_path="$1"
 fi
